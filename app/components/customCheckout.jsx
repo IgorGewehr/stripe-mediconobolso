228e38735889fb7aa254e0e07b493f8a1229112b
IgorGewehr <gewehr.py@gmail.com>
2025-06-23 15:32:12 -0300

    hotfix

--- app/components/customCheckout.jsx
+++ app/components/customCheckout.jsx
@@ -2158,17 +2158,17 @@ export default function CustomCheckout() {
             const script = document.createElement('script');
             script.id = 'facebook-pixel-script';
             script.innerHTML = `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1033180232110037');
-            fbq('track', 'InitiateCheckout');
-        `;
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}'); // Use variável de ambiente
+        fbq('track', 'InitiateCheckout');
+    `;
             document.head.appendChild(script);
 
             // Add noscript pixel
@@ -2177,7 +2177,7 @@ export default function CustomCheckout() {
             img.height = 1;
             img.width = 1;
             img.style.display = 'none';
-            img.src = 'https://www.facebook.com/tr?id=1033180232110037&ev=InitiateCheckout&noscript=1';
+            img.src = `https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}&ev=InitiateCheckout&noscript=1`;
             noscript.appendChild(img);
             document.head.appendChild(noscript);
         }
